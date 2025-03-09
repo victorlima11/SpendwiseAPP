ca2d6cbb1f8281d93645b85c4e8c2fef855bb991
victorlima11 <victorconta1009@gmail.com>
2025-03-09 12:07:24 -0300

    ajustes botões

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -49,11 +49,15 @@ function Home() {
       <div className="hero">
         <div id="box"></div> {/* Aqui está o fundo borrado */}
         <div className="hero-text">
-          <h1>Controle Suas Finanças com o <span className="highlight">Spendwise</span></h1>
+          <h1>Controle Suas Finanças com o <span className="highlight">Spendwise.</span></h1>
           <p>Gerencie seus gastos, economize mais e alcance seus objetivos financeiros com facilidade. Organize seu orçamento, acompanhe suas despesas e visualize gráficos de progresso para saber exatamente onde está o seu dinheiro. Com o Spendwise, você pode tomar decisões mais inteligentes e alcançar a saúde financeira de forma eficiente e prática.</p>
           <div className="buttons">
-            <button className="button-start">Começar</button>
-            <button className="button-plans">Planos</button>
+            <button className="button-start learn-more">
+              <span className="circle">
+                <span className="icon arrow"></span>
+              </span>
+              <span className="button-text">Começar</span>
+            </button>
           </div>
         </div>
       </div>
