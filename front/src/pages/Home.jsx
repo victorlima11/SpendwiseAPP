6a23bfa22aa82b70415c0108b46ae686d8a6befe
victorlima11 <victorconta1009@gmail.com>
2025-03-10 16:18:15 -0300

    10/03/2025

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './home.css';
-import logo from '/src/assets/spendwisr.png';
-import celular from '/src/assets/celular.png';
-
+import coin from "/src/assets/coin.png"
 // Importando o módulo do efeito de gradiente borrado
 import { BlurGradientBg } from '../assets/BlurGradientBg.module.js';
 
@@ -23,12 +21,9 @@ function Home() {
     console.log('Script de fundo borrado executado');
     new BlurGradientBg({
       dom: "box",
-      colors: ["#000000","#0d2a59","#000000","#416081"],
+      colors: ["#000000", "#0d2a59", "#000000", "#416081"],
       loop: true
     });
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   return (
@@ -45,30 +40,39 @@ function Home() {
         </div>
       </nav>
 
-      {/* Seção Hero com o fundo borrado */}
       <div className="hero">
-        <div id="box"></div> {/* Aqui está o fundo borrado */}
+        <div id="box"></div>
         <div className="hero-text">
           <h1>Controle Suas Finanças com o <span className="highlight">Spendwise.</span></h1>
           <p>Gerencie seus gastos, economize mais e alcance seus objetivos financeiros com facilidade. Organize seu orçamento, acompanhe suas despesas e visualize gráficos de progresso para saber exatamente onde está o seu dinheiro. Com o Spendwise, você pode tomar decisões mais inteligentes e alcançar a saúde financeira de forma eficiente e prática.</p>
           <div className="buttons">
-            <button className="button-start learn-more">
+            <Link className="button-start learn-more" to="/dashboard">
               <span className="circle">
                 <span className="icon arrow"></span>
               </span>
               <span className="button-text">Começar</span>
-            </button>
+            </Link>
           </div>
         </div>
       </div>
 
-      {/* Segunda Seção com fundo diferente */}
-      <div className="new-section">
-        <div className="new-section-content">
-          <h2>Explore Nossos Planos</h2>
-          <p>Escolha o plano que melhor se adapta às suas necessidades financeiras. O Spendwise oferece soluções para todos os tipos de usuário!</p>
-          <button className="button-new-section">Saiba Mais</button>
-        </div>
+      <div className="features-container">
+          <div className="features-title">
+            <h1 className="feature-h1">Features</h1>
+            <p>Esses são os serviços disponíveis por nosso software.</p>
+          </div>
+          <div className="feature-cards">
+              <div className="mini-title">
+                <img className="card-image" src={coin} alt="coin"/>
+                <p>Teste</p>
+              </div>
+              <div className="feature-card-title">
+                <h1>Título do card</h1>
+              </div>
+              <div className="feature-card-content">
+                <p>Esse é o teste de card.</p>
+              </div>
+          </div>
       </div>
     </div>
   );
