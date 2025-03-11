eb21be2375f4c509b7fff3f769b1efa01342571a
Victor <victorconta1009@gmail.com>
2025-03-11 19:17:51 -0300

    fix

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Navbar from '/src/pages/components/Navbar';
 import FeaturesSection from './components/FeaturesSection';
 import HeroSection from './components/HeroSection';
-import './home.css'
+import '/src/pages/components/styles/home.css'
 
 function Home() {
   return (
