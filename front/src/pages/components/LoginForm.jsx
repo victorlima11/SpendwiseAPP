f6ec1a072934c721f734e1d77fa8a3c8ed2b34ca
Victor <victorconta1009@gmail.com>
2025-03-12 11:17:15 -0300

    Formulario login responsivo

--- front/src/pages/components/LoginForm.jsx
+++ front/src/pages/components/LoginForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import '/src/pages/components/styles/loginForm.css'
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
@@ -9,11 +11,9 @@ const LoginForm = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-
     try {
       const response = await axios.post("http://localhost:8000/users/login/", { email, password });
       const { access_token } = response.data;
-
       if (access_token) {
         localStorage.setItem("token", access_token);
         navigate("/dashboard");
@@ -26,26 +26,34 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="login-form-container">
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Senha"
-          required
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Entrar</button>
-      </form>
-      <p>Não tem uma conta? <a href="#">Cadastre-se</a></p>
+    <div className="login-container">
+      <div className="login-card">
+        <div className="logo-form">
+          <h1>Spendwise.</h1>
+        </div>
+        <h2>Login</h2>
+        <form className="login-form" onSubmit={handleLogin}>
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="forgot-password">
+              <a href="/reset-password">Esqueceu a senha?</a>
+          </div>
+          <button type="submit">Entrar</button>
+        </form>
+        <p>Não tem uma conta? <a className="register" href="/register">Cadastre-se</a></p>
+      </div>
     </div>
   );
 };
