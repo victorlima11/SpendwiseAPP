490867bcd67d492f4ba51c9c823b0d101417afa0
Victor <victorconta1009@gmail.com>
2025-03-15 21:34:44 -0300

    fix

--- front/src/pages/AddTransaction.jsx
+++ front/src/pages/AddTransaction.jsx
@@ -1,20 +1,18 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './addTransaction.css';
+import './addTransactionModal.css';
 
-function AddTransaction() {
-  const [tipo, setTipo] = useState('despesa'); // Valor inicial é 'despesa'
+function AddTransactionModal({ showModal, closeModal }) {
+  const [tipo, setTipo] = useState('despesa');
   const [amount, setAmount] = useState('');
   const [category, setCategory] = useState('');
   const [description, setDescription] = useState('');
   const [date, setDate] = useState('');
   const [error, setError] = useState('');
 
-  // Função para lidar com o envio do formulário
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Verificar se todos os campos estão preenchidos
     if (!amount || !category || !date) {
       setError('Por favor, preencha todos os campos.');
       return;
@@ -28,110 +26,112 @@ function AddTransaction() {
     };
 
     try {
-      // Determinar a URL com base no tipo (despesa ou rendimento)
       const url = tipo === 'despesa' ? 'http://localhost:8000/users/despesas/' : 'http://localhost:8000/users/rendimentos/';
-
-      // Enviar os dados da transação para a API
+      
       await axios.post(url, transactionData, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`,
         },
       });
 
-      // Resetar os campos do formulário após o envio
       setAmount('');
       setCategory('');
       setDescription('');
       setDate('');
       setError('');
       alert('Transação adicionada com sucesso!');
+      closeModal();
     } catch (error) {
       setError('Ocorreu um erro ao adicionar a transação. Tente novamente.');
     }
   };
 
   return (
-    <div className="add-transaction-container">
-      <h1>Adicionar Transação</h1>
-      <form onSubmit={handleSubmit}>
-        {/* Tipo de Transação: Despesa ou Rendimento */}
-        <div className="input-group">
-          <label>Tipo de Transação:</label>
-          <div>
-            <label>
+    showModal && (
+      <div className="modal-overlay">
+        <div className="modal-container">
+          <button className="modal-close-button" onClick={closeModal}>X</button>
+          <h2 className="modal-title">Adicionar Transação</h2>
+          <form onSubmit={handleSubmit} className="add-transaction-form">
+            <div className="add-transaction-input-group">
+              <label className="add-transaction-label">Tipo de Transação:</label>
+              <div className="add-transaction-radio-group">
+                <label className="add-transaction-radio-label">
+                  <input 
+                    type="radio" 
+                    name="tipo" 
+                    value="despesa" 
+                    checked={tipo === 'despesa'} 
+                    onChange={() => setTipo('despesa')} 
+                    className="add-transaction-radio-input"
+                  />
+                  Despesa
+                </label>
+                <label className="add-transaction-radio-label">
+                  <input 
+                    type="radio" 
+                    name="tipo" 
+                    value="rendimento" 
+                    checked={tipo === 'rendimento'} 
+                    onChange={() => setTipo('rendimento')} 
+                    className="add-transaction-radio-input"
+                  />
+                  Rendimento
+                </label>
+              </div>
+            </div>
+
+            <div className="add-transaction-input-group">
+              <label className="add-transaction-label">Montante:</label>
               <input 
-                type="radio" 
-                name="tipo" 
-                value="despesa" 
-                checked={tipo === 'despesa'} 
-                onChange={() => setTipo('despesa')} 
+                type="number" 
+                value={amount} 
+                onChange={(e) => setAmount(e.target.value)} 
+                placeholder="Valor da transação"
+                className="add-transaction-input"
               />
-              Despesa
-            </label>
-            <label>
+            </div>
+
+            <div className="add-transaction-input-group">
+              <label className="add-transaction-label">Categoria:</label>
               <input 
-                type="radio" 
-                name="tipo" 
-                value="rendimento" 
-                checked={tipo === 'rendimento'} 
-                onChange={() => setTipo('rendimento')} 
+                type="text" 
+                value={category} 
+                onChange={(e) => setCategory(e.target.value)} 
+                placeholder="Ex: Alimentação, Salário"
+                className="add-transaction-input"
               />
-              Rendimento
-            </label>
-          </div>
+            </div>
+
+            <div className="add-transaction-input-group">
+              <label className="add-transaction-label">Descrição (opcional):</label>
+              <input 
+                type="text" 
+                value={description} 
+                onChange={(e) => setDescription(e.target.value)} 
+                placeholder="Ex: Compra no supermercado"
+                className="add-transaction-input"
+              />
+            </div>
+
+            <div className="add-transaction-input-group">
+              <label className="add-transaction-label">Data:</label>
+              <input 
+                type="date" 
+                value={date} 
+                onChange={(e) => setDate(e.target.value)} 
+                className="add-transaction-input"
+              />
+            </div>
+
+            {error && <p className="add-transaction-error">{error}</p>}
+
+            <button type="submit" className="add-transaction-submit-button">Adicionar Transação</button>
+          </form>
         </div>
-
-        {/* Montante */}
-        <div className="input-group">
-          <label>Montante:</label>
-          <input 
-            type="number" 
-            value={amount} 
-            onChange={(e) => setAmount(e.target.value)} 
-            placeholder="Valor da transação"
-          />
-        </div>
-
-        {/* Categoria */}
-        <div className="input-group">
-          <label>Categoria:</label>
-          <input 
-            type="text" 
-            value={category} 
-            onChange={(e) => setCategory(e.target.value)} 
-            placeholder="Ex: Alimentação, Salário"
-          />
-        </div>
-
-        {/* Descrição */}
-        <div className="input-group">
-          <label>Descrição (opcional):</label>
-          <input 
-            type="text" 
-            value={description} 
-            onChange={(e) => setDescription(e.target.value)} 
-            placeholder="Ex: Compra no supermercado"
-          />
-        </div>
-
-        {/* Data */}
-        <div className="input-group">
-          <label>Data:</label>
-          <input 
-            type="date" 
-            value={date} 
-            onChange={(e) => setDate(e.target.value)} 
-          />
-        </div>
-
-        {/* Erro de validação */}
-        {error && <p className="error">{error}</p>}
-
-        {/* Botão para enviar a transação */}
-        <button type="submit" className="button-submit">Adicionar Transação</button>
-      </form>
-    </div>
+      </div>
+    )
   );
 }
 
-export default AddTransaction;
+export default AddTransactionModal;
