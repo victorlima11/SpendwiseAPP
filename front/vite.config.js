006bd12a1a957e4da97ef54a0bf1d92f76bc0ca2
Victor <victorconta1009@gmail.com>
2025-03-19 23:46:49 -0300

    Update vite.config.js

--- front/vite.config.js
+++ front/vite.config.js
@@ -7,5 +7,20 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173
+  },
+  build: {
+    // Aumenta o limite de tamanho de chunk para evitar o aviso
+    chunkSizeWarningLimit: 1000, // Aumente esse valor para 1MB (por exemplo)
+
+    // Exemplo de uso de manualChunks para dividir dependências
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Divida as dependências de node_modules em um arquivo separado
+          }
+        }
+      }
+    }
   }
 })
