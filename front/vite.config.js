712667b2176d8002ae73a74c785fcbae10c94b42
Victor <victorconta1009@gmail.com>
2025-03-20 00:17:05 -0300

    fix

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,15 +8,14 @@ export default defineConfig({
     port: 5173
   },
   build: {
-    // Aumenta o limite de tamanho de chunk para evitar o aviso
-    chunkSizeWarningLimit: 1000, // Aumente esse valor para 1MB (por exemplo)
-
-    // Exemplo de uso de manualChunks para dividir dependências
+    minify: false,
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
+      external: ['seedrandom'],
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return 'vendor'; // Divida as dependências de node_modules em um arquivo separado
+            return 'vendor';
           }
         }
       }
